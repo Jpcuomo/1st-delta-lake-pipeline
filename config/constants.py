4e38da9da153e117206b5e3675211f724e27c524
Jpcuomo <jpcuomo2000@gmail.com>
2025-08-28 13:53:30 -0300

    se agrega COLS al init

--- config/constants.py
+++ config/constants.py
@@ -1,15 +1,15 @@
 # Nombre de columnas para binance/klines
 
-COLS = mapping_columns = {
-    '0':"open_time",
-    '1':"open",
-    '2':"high",
-    '3':"low",
-    '4':"close",
-    '5':"volume",
-    '6':"close_time",
-    '7':"quote_asset_volume",
-    '8':"num_trades",
-    '9':"tb_base_asset_volume",
-    '10':"tb_quote_asset_volume",
-    '11':"ignore"}
+COLS = {
+    0:"open_time",
+    1:"open",
+    2:"high",
+    3:"low",
+    4:"close",
+    5:"volume",
+    6:"close_time",
+    7:"quote_asset_volume",
+    8:"num_trades",
+    9:"tb_base_asset_volume",
+    10:"tb_quote_asset_volume",
+    11:"ignore"}
